d74c03d845f61584a536e8a5ac5c79456587c4e0
elBanna00 <moaazibrahim3@gmail.com>
2024-05-25 13:15:24 +0300

    pass stage2

--- app/server.go
+++ app/server.go
@@ -16,9 +16,29 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	con, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	var buff []byte
+
+	_, err = con.Read(buff)
+	if err != nil {
+
+		fmt.Println("Error reading message: ", err.Error())
+
+		os.Exit(1)
+
+	}
+
+	_, err = con.Write([]byte("+PONG\r\n"))
+
+	if err != nil {
+
+		fmt.Println("Error writing response message: ", err.Error())
+	}
+
 }
